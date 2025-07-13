16602a34c86b8f2169352ba780b5d7a7e5439570
Fil Daniil <irarbc89@gmail.com>
2025-07-13 14:19:14 +0300

    fix: initialize tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: 'oklch(0.922 0 0)',
+        ring: 'oklch(0.708 0 0)',
+      },
+      borderColor: {
+        DEFAULT: 'oklch(0.922 0 0)',
+      },
+    },
   },
   plugins: [],
+  darkMode: 'class',
 };
-
-export default config;
